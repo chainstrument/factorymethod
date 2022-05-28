c528068f20f39e32a19861f06b1b60e257256fd4
chainstrument <minitutoriel@gmail.com>
2022-05-28 17:34:39 +0200

    implementation

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php 
 
+$position = new Position(15, 5);
+
+
+$shape = new ShapeFactory();
+$shape->create('Rectangle', $position);
+
+$position = new Position(25, 5);
+$shape->create('Circle', $position);
